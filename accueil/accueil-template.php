1e75406f0c6cc0deb8f8fac4f44f5a6b8e61a6da
paulfages9 <33320829+paulfages9@users.noreply.github.com>
2018-03-19 15:40:49 +0100

    branche accueil

---
+++ accueil/accueil-template.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title><?= $pageTitle ?></title>
+    </head>
+    <body>
+        <?= $pageContent ?>
+    </body>
+</html>
---
+++ accueil/accueil.php
@@ -0,0 +1,7 @@
+<?php
+
+$pageTitle = 'Accueil Bomberman';
+$pageContent = '<p>Bienvenue sur Bomberman</p>';
+
+//require genere fatal error si le fichier n'existe pas
+require('accueil-template.php');
